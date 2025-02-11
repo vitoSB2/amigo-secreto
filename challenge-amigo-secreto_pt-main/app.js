4fc2752713a9c98bb359a81ec0385b247af5072d
vitoSB2 <vitorsantiagobarreto123@gmail.com>
2025-02-11 10:29:32 -0300

    primeiro commit

---
+++ challenge-amigo-secreto_pt-main/app.js
@@ -0,0 +1,46 @@
+let listaDeNomes = [];
+let quantidadeDeNomes = 0;
+
+function adicionarAmigo(){
+
+    let amigo = document.querySelector("input").value;
+
+    if(amigo == ""){
+        alert("Digite um nome!");
+    } else {
+        listaDeNomes.push(amigo);
+        quantidadeDeNomes++;
+        atualizarListaDeNomes();
+        limparCampo();
+    }
+
+}
+
+function sortearAmigo(){
+
+    let indexAmigoSorteado = parseInt(Math.random() * quantidadeDeNomes);
+    let amigoSorteado = listaDeNomes[indexAmigoSorteado];
+
+    exibirAmigoSorteado(amigoSorteado);
+}
+
+function limparCampo(){
+
+    let campo = document.querySelector("input");
+    campo.value = "";
+}
+
+function atualizarListaDeNomes(){
+
+    let listaAmigos = document.querySelector("#listaAmigos");
+    listaAmigos.innerHTML = listaDeNomes.map(nome => "<li>" + nome + "</li>").join("");
+}
+
+function exibirAmigoSorteado(nomeSorteado){
+
+    let resultado = document.querySelector("#resultado");
+    resultado.innerHTML = "O Amigo Secreto Sorteado é: " + nomeSorteado;
+
+    let listaAmigos = document.querySelector("#listaAmigos");
+    listaAmigos.innerHTML = "";
+}
